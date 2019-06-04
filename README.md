39bb883a35c8cf48bbdc1756c05e09bc072498b6
sambhaji k <sambhaji.kolate@elastic.run>
2019-06-04 15:32:09 +0530

    added angualr demo project

--- README.md
+++
@@ -1,1 +0,0 @@
-# sbkolate.github.io
---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+$(function () {
+
+  // Textarea Auto Resize
+  autosize($('#autosize'));
+
+  // Select2 Box
+  $('#select1, #select2, #select3').select2();
+  $("#select4").select2({ maximumSelectionLength: 2 });
+  $("#select5").select2({ minimumResultsForSearch: Infinity });
+  $("#select6").select2({ tags: true });
+
+  // Toggles
+  $('.toggle').toggles({
+    on: true,
+    height: 26
+  });
+
+  // Input Masks
+  $("#date").mask("99/99/9999");
+  $("#phone").mask("(999) 999-9999");
+  $("#ssn").mask("999-99-9999");
+
+  // Date Picker
+  $('#datepicker').datepicker();
+  $('#policyStartdatepicker').datepicker();
+  $('#policyEnddatepicker').datepicker();
+  $('#datepicker-inline').datepicker();
+  $('#datepicker-multiple').datepicker({ numberOfMonths: 2 });
+
+  // Time Picker
+  $('#tpBasic').timepicker();
+  $('#tp2').timepicker({ 'scrollDefault': 'now' });
+  $('#tp3').timepicker();
+
+  $('#setTimeButton').on('click', function () {
+    $('#tp3').timepicker('setTime', new Date());
+  });
+
+  // Colorpicker
+  $('#colorpicker1').colorpicker();
+  $('#colorpicker2').colorpicker({
+    customClass: 'colorpicker-lg',
+    sliders: {
+      saturation: {
+        maxLeft: 200,
+        maxTop: 200
+      },
+      hue: { maxTop: 200 },
+      alpha: { maxTop: 200 }
+    }
+  });
+
+});
